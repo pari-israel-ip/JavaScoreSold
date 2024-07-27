7768aae59790ea4d6c837ca584848b1d0e4f14dc
IP-lost <israelpr2020@gmail.com>
2024-07-27 09:05:15 -0400

    PruebasUnitarias01

--- 03-control-flujo/05-do-while.js
+++ 03-control-flujo/05-do-while.js
@@ -1,7 +1,7 @@
 //cuales son lso numeros pares
 
 let i = 2;
-while ( i < 2 ) {
+while ( i < 10 ) {
 
     if ( i % 2 == 0 ){
         console.log('Numero par', i);
--- 03-control-flujo/07-for-of.js
+++ 03-control-flujo/07-for-of.js
@@ -2,17 +2,39 @@
 //do while
 //for
 
-let animales = ['Chanchito','Dragon','Perrito'];
+// let animales = ['Chanchito','Dragon','Perrito'];
 
-for (let animal of animales)
-{
-    console.log(animal);
+// for (let animal of animales)
+// {
+//     console.log(animal);
+// }
+
+
+// let i = 0;
+// while (i < animales.length){
+
+//     console.log(animales[i]);
+//     i++;
+// }
+
+// let orden = [88, 5, 6, 15, -5, -239, 44];
+
+// function ordenarMayoryMenor(arr){
+   
+// //return arr.sort((a, b) => a - b)
+// //return arr.filter(numero => numero > 0)
+   
+        
+// }
+
+// let resultado = ordenarMayoryMenor(orden);
+// console.log(resultado);
+
+function precio(precio,impuesto){
+    let impuestototal = precio * impuesto;
+    let total = precio + impuestototal;
+    return total;
 }
 
-
-let i = 0;
-while (i < animales.length){
-
-    console.log(animales[i]);
-    i++;
-}
+let resultado =precio(19.90,0.15);
+console.log(resultado);
